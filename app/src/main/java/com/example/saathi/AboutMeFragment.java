a835d4d2daeabaad8a17bf8f7a5d8984886b2c3f
Kshitij0910 <jainkshitij01@gmail.com>
2020-05-11 12:36:55 +0530

    1)FireBaseAuth 2)Navigation Drawer Activity 3)Logout in Nav_drawer 4)Fragments created 5)About me Layout in progress 6)DatePicker added

---
+++ app/src/main/java/com/example/saathi/AboutMeFragment.java
@@ -0,0 +1,57 @@
+package com.example.saathi;
+
+
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AutoCompleteTextView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+
+
+public class AboutMeFragment extends Fragment  implements DatePickerDialog.OnDateSetListener {
+
+    TextView selectDate;
+
+    public AboutMeFragment(){
+
+    }
+
+
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view= inflater.inflate(R.layout.frag_about_me, container, false);
+        selectDate=(TextView)view.findViewById(R.id.dob);
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment newFragment=new DatePickerFragment();
+                newFragment.setTargetFragment(AboutMeFragment. this, 0);
+                newFragment.show(getActivity().getSupportFragmentManager(), "datePicker");
+            }
+        });
+        return view;
+
+
+
+        }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        StringBuilder sb=new StringBuilder().append(dayOfMonth).append("/").append(month).append("/").append(year);
+        String formattedDate=sb.toString();
+        selectDate.setText(formattedDate);
+    }
+}
+
